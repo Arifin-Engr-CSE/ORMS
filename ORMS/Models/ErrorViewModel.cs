1b4e49980f26aa9c17117feeb5c4c35544662c97
arifi <arifi@ARIFIN>
2021-11-05 00:06:46 +0600

    Add project files.

---
+++ ORMS/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ORMS.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
